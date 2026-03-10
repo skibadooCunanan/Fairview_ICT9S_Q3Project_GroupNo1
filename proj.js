834358fc546d78e7069eeb9e460901d307c5334b
skibadooCunanan <cunanan.dt@obmontessori.edu.ph>
2026-03-10 19:05:05 +0800

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,24 @@
+function calculateFare() {
+
+    let pickup = parseFloat(document.getElementById("pickup").value);
+    let dropoff = parseFloat(document.getElementById("dropoff").value);
+    let basefare = 50;
+    let perKMrate = 15;
+    let baseKM = 2;
+    let discount = document.getElementById("disc").checked;
+
+    let distance = Math.max(Math.abs(dropoff - pickup), 0);
+    let extraKM = Math.max(distance - baseKM, 0);
+
+    let fare = basefare + extraKM * perKMrate;
+
+    fare = fare * (1 - 0.2 * discount);
+
+    document.getElementById("DisplayAns").innerHTML =
+        "Total Fare: ₱ " + fare.toFixed(2);
+    
+    if(pickup === dropoff)
+    document.getElementById("DisplayAns").innerHTML =
+    "Pickup and Dropoff are the same. Fare: ₱0.00";
+    return;
+}
